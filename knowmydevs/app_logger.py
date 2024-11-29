494499c960bf2a54e0fb1c522dd80fd2cee65e0f
Rafael de Oliveira Marques <rafaelomarques@gmail.com>
2024-11-29 08:51:58 -0300

    Set urllib3 log to ERROR

--- knowmydevs/app_logger.py
+++ knowmydevs/app_logger.py
@@ -4,6 +4,7 @@ from knowmydevs.core.config import app_config
 
 logging.basicConfig()
 logging.getLogger().setLevel(app_config.log_level)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
 
 logger = logging.getLogger("knowmydevs")
 
