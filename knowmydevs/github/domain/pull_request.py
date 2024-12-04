9a4dd0bfa5140b1c4f36e3f1e1422cbba4c713c9
Rafael de Oliveira Marques <rafaelomarques@gmail.com>
2024-12-04 10:02:00 -0300

    Add Install Info to PR

--- knowmydevs/github/domain/pull_request.py
+++ knowmydevs/github/domain/pull_request.py
@@ -13,20 +13,19 @@ class PullRequest(SQLModel, table=True):
         ..., sa_type=BigInteger, foreign_key="installations.id"
     )
 
+    closed_at: datetime.datetime | None = Field(None)
+    created_at: datetime.datetime
+    merge_commit_sha: str | None = Field(None, max_length=200)
     merged: bool
+    merged_at: datetime.datetime | None = Field(None)
     merged_by_id: int | None = Field(None, sa_type=BigInteger, index=True)
-    merged_by_login: str = Field(..., max_length=150)
+    merged_by_login: str | None = Field(None, max_length=150)
     number: int = Field(..., sa_type=BigInteger)
     opened_by_id: int = Field(sa_type=BigInteger, index=True)
     opened_by_login: str = Field(..., max_length=150)
-    state: PullRequestState = Field(sa_type=String, max_length=10)
-    title: str = Field(max_length=500)
-    url: str = Field(max_length=500)
-
-    created_at: datetime.datetime
-    updated_at: datetime.datetime | None = Field(None)
-    closed_at: datetime.datetime | None = Field(None)
-    merged_at: datetime.datetime | None = Field(None)
-    merge_commit_sha: str | None = Field(None, max_length=200)
     repository_id: int = Field(..., sa_type=BigInteger, index=True)
     repository_name: str = Field(..., max_length=250, index=True)
+    state: PullRequestState = Field(sa_type=String, max_length=10)
+    title: str = Field(max_length=500)
+    updated_at: datetime.datetime | None = Field(None)
+    url: str = Field(max_length=500)
